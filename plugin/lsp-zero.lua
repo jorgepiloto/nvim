0597335bed26b19d8081f5aff2f60a5e6eec8ae1
Jorge Martinez <jorge.martinezgarrido@ansys.com>
2023-02-24 21:04:40 +0100

    feat(plugin): add LSP configuration

--- plugin/lsp-zero.lua
+++ plugin/lsp-zero.lua
@@ -2,3 +2,54 @@ local lsp = require("lsp-zero")
 
 lsp.preset("recommended")
 lsp.setup()
+
+
+local kind_icons = {
+  Text = "",
+  Method = "",
+  Function = "",
+  Constructor = "",
+  Field = "",
+  Variable = "",
+  Class = "ﴯ",
+  Interface = "",
+  Module = "",
+  Property = "ﰠ",
+  Unit = "",
+  Value = "",
+  Enum = "",
+  Keyword = "",
+  Snippet = "",
+  Color = "",
+  File = "",
+  Reference = "",
+  Folder = "",
+  EnumMember = "",
+  Constant = "",
+  Struct = "",
+  Event = "",
+  Operator = "",
+  TypeParameter = ""
+}
+
+
+
+-- Declare the options for the LSP completion popup
+local cmp = require("cmp")
+cmp.setup {
+  formatting = {
+    format = function(entry, vim_item)
+      -- Kind icons
+      vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+      -- Source
+      vim_item.menu = ({
+        buffer = "[Buffer]",
+        nvim_lsp = "[LSP]",
+        luasnip = "[LuaSnip]",
+        nvim_lua = "[Lua]",
+        latex_symbols = "[LaTeX]",
+      })[entry.source.name]
+      return vim_item
+    end
+  },
+}
